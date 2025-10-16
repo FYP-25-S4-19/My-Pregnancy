8bc324d41e283edca34595296379a00fb214309a
Alexander Neo <AlexanderNeo427@gmail.com>
2025-10-17 00:05:45 +0800

    Fixed some issue relating to module imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from app.entities.educational_articles.edu_articles_router import edu_articles_router
 
-from app.entities.items.items_router import items_router
 
 app = FastAPI()
-app.include_router(items_router)
+app.include_router(edu_articles_router)
+
 
 @app.get("/")
 def index():
