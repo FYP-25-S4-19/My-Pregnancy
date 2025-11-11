2585c8cdaa8c19ee6a1bf2ed252645ac88417cf1
Alexander Neo <AlexanderNeo427@gmail.com>
2025-11-11 18:30:23 +0800

    Trying a new script to nuke the DB

--- backend/app/db/nuke_db.py
+++ backend/app/db/nuke_db.py
@@ -1,12 +1,12 @@
 from app.db.db_config import engine
-from app.db.db_schema import Base
 from sqlalchemy import text
 
 if __name__ == "__main__":
     try:
         with engine.connect() as conn:
-            conn.execute(text("DROP TABLE IF EXISTS alembic_version CASCADE;"))
-            conn.commit()
-        Base.metadata.drop_all(engine)
+            print("Dropping schema public CASCADE...")
+            conn.execute(text("DROP SCHEMA public CASCADE;"))
+            print("Recreating schema public...")
+            conn.execute(text("CREATE SCHEMA public;"))
     except Exception as e:
         print(f"Exception occurred while nuking database: {e}")
