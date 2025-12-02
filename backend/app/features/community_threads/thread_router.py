2f113dd883cd98186fdbe26b64d1966023939677
Alexander Neo <AlexanderNeo427@gmail.com>
2025-12-02 15:58:20 +0800

    WIP commits for some work on the Community Threads

--- backend/app/features/community_threads/thread_router.py
+++ backend/app/features/community_threads/thread_router.py
@@ -1,16 +1,57 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.users_manager import current_active_user
 from app.db.db_config import get_db
+from app.db.db_schema import User
+from app.features.community_threads.thread_models import (
+    CreateThreadData,
+    ThreadData,
+    ThreadPreviewData,
+    ThreadUpdateData,
+)
 from app.features.community_threads.thread_service import ThreadService
 
-thread_router = APIRouter(prefix="/threads", tags=["Community Threads"])
+community_threads_router = APIRouter(prefix="/threads", tags=["Community Threads"])
 
 
 def get_threads_service(db: AsyncSession = Depends(get_db)) -> ThreadService:
     return ThreadService(db)
 
 
-@thread_router.get("/")
-async def get_thread_previews(service: ThreadService = Depends(get_threads_service)):
+@community_threads_router.get("/", response_model=list[ThreadPreviewData])
+async def get_thread_previews(service: ThreadService = Depends(get_threads_service)) -> list[ThreadPreviewData]:
     return await service.get_thread_previews()
+
+
+@community_threads_router.get("/{thread_id}", response_model=ThreadData)
+async def get_thread_by_id(thread_id: int, service: ThreadService = Depends(get_threads_service)) -> ThreadData:
+    return await service.get_thread_by_id(thread_id)
+
+
+@community_threads_router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_thread(
+    thread_data: CreateThreadData,
+    service: ThreadService = Depends(get_threads_service),
+    creator: User = Depends(current_active_user),
+) -> None:
+    await service.create_thread(thread_data, creator)
+
+
+@community_threads_router.put("/{thread_id}", status_code=status.HTTP_200_OK)
+async def update_thread(
+    thread_id: int,
+    thread_data: ThreadUpdateData,
+    service: ThreadService = Depends(get_threads_service),
+    current_user: User = Depends(current_active_user),
+) -> None:
+    await service.update_thread(thread_id, thread_data, current_user)
+
+
+@community_threads_router.delete("/{thread_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_thread(
+    thread_id: int,
+    service: ThreadService = Depends(get_threads_service),
+    current_user: User = Depends(current_active_user),
+) -> None:
+    await service.delete_thread(thread_id, current_user)
--- backend/app/features/community_threads/thread_service.py
+++ backend/app/features/community_threads/thread_service.py
@@ -1,8 +1,17 @@
+from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload, selectinload
+from starlette import status
 
-from app.db.db_schema import CommunityThread
-from app.features.community_threads.thread_models import ThreadPreview
+from app.db.db_schema import CommunityThread, ThreadComment, User
+from app.features.community_threads.thread_models import (
+    CreateThreadData,
+    ThreadCommentData,
+    ThreadData,
+    ThreadPreviewData,
+    ThreadUpdateData,
+)
 from app.shared.utils import format_user_fullname
 
 
@@ -10,11 +19,11 @@ class ThreadService:
     def __init__(self, db: AsyncSession):
         self.db = db
 
-    async def get_thread_previews(self) -> list[ThreadPreview]:
-        stmt = select(CommunityThread).limit(10).order_by(CommunityThread.posted_at.desc())
+    async def get_thread_previews(self) -> list[ThreadPreviewData]:
+        stmt = select(CommunityThread).order_by(CommunityThread.posted_at.desc())
         query_results = (await self.db.execute(stmt)).scalars().all()
         return [
-            ThreadPreview(
+            ThreadPreviewData(
                 id=thread.id,
                 creator_name=format_user_fullname(thread.creator),
                 title=thread.title,
@@ -24,14 +33,74 @@ class ThreadService:
             for thread in query_results
         ]
 
-    async def get_thread_by_id(self, thread_id: int):
-        pass
+    async def get_thread_by_id(self, thread_id: int) -> ThreadData:
+        stmt = (
+            select(CommunityThread)
+            .where(CommunityThread.id == thread_id)
+            .options(
+                joinedload(CommunityThread.creator),
+                selectinload(CommunityThread.comments).joinedload(ThreadComment.commenter),
+            )
+        )
+        thread_result = (await self.db.execute(stmt)).scalar_one_or_none()
+        if not thread_result:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Thread not found")
+        return ThreadData(
+            id=thread_result.id,
+            creator_id=thread_result.creator.id,
+            creator_fullname=format_user_fullname(thread_result.creator),
+            title=thread_result.title,
+            content=thread_result.content,
+            posted_at=thread_result.posted_at,
+            comments=[
+                ThreadCommentData(
+                    id=comment.id,
+                    thread_id=comment.thread_id,
+                    commenter_id=comment.commenter_id,
+                    commenter_fullname=format_user_fullname(comment.commenter),
+                    commented_at=comment.commented_at,
+                    content=comment.content,
+                )
+                for comment in thread_result.comments
+            ],
+        )
 
-    async def create_thread(self, thread_data):
-        pass
+    async def create_thread(self, thread_data: CreateThreadData, creator: User) -> None:
+        new_thread = CommunityThread(
+            creator_id=creator.id,
+            title=thread_data.title,
+            content=thread_data.content,
+        )
+        self.db.add(new_thread)
+        await self.db.commit()
+        await self.db.refresh(new_thread)
+        new_thread.creator = creator
 
-    async def update_thread(self, thread_id, thread_data):
-        pass
+    async def update_thread(self, thread_id: int, thread_data: ThreadUpdateData, current_user: User) -> None:
+        stmt = select(CommunityThread).where(CommunityThread.id == thread_id)
+        thread_result = (await self.db.execute(stmt)).scalar_one_or_none()
 
-    async def delete_thread(self, thread_id):
-        pass
+        if not thread_result:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Thread not found")
+
+        if thread_result.creator_id != current_user.id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to update this thread")
+
+        thread_result.title = thread_data.title
+        thread_result.content = thread_data.content
+
+        await self.db.commit()
+        await self.db.refresh(thread_result)
+
+    async def delete_thread(self, thread_id: int, current_user: User) -> None:
+        stmt = select(CommunityThread).where(CommunityThread.id == thread_id)
+        thread_result = (await self.db.execute(stmt)).scalar_one_or_none()
+
+        if not thread_result:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Thread not found")
+
+        if thread_result.creator_id != current_user.id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete this thread")
+
+        await self.db.delete(thread_result)
+        await self.db.commit()
